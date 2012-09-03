81b87ecd2ebe057ca93a1f36df8a6032e807ceda
posohin <niko.exe@gmail.com>
2012-09-03 14:35:24 +0400

    Added Users

---
+++ protected/views/users/_form.php
@@ -0,0 +1,57 @@
+<?php
+/* @var $this UsersController */
+/* @var $model Users */
+/* @var $form CActiveForm */
+?>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'users-form',
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'login'); ?>
+		<?php echo $form->textField($model,'login',array('size'=>60,'maxlength'=>255)); ?>
+		<?php echo $form->error($model,'login'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'password'); ?>
+		<?php echo $form->passwordField($model,'password',array('size'=>60,'maxlength'=>255)); ?>
+		<?php echo $form->error($model,'password'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'name'); ?>
+		<?php echo $form->textField($model,'name',array('size'=>60,'maxlength'=>255)); ?>
+		<?php echo $form->error($model,'name'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'email'); ?>
+		<?php echo $form->textField($model,'email',array('size'=>60,'maxlength'=>255)); ?>
+		<?php echo $form->error($model,'email'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'role'); ?>
+		<?php echo $form->dropDownList($model, 'role',
+	 		array('admin' => 'Администратор', 'worker' => 'Работник'),
+	 		array('style' => 'width: 320px !important', 'placeholder' => 'Тип учетной записи'))
+		?>
+		<?php echo $form->error($model,'role'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
